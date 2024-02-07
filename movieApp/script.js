28b9e18e8d315f7e3b426ab6fb46489fd3b40160
ricaim16 <128908127+ricaim16@users.noreply.github.com>
2024-02-07 21:45:48 +0300

    Update script.js

--- movieApp/script.js
+++ movieApp/script.js
@@ -20,41 +20,11 @@ form.addEventListener('submit', (e) => {
 });
 
 
-const genres = [
-  { name: 'Action', id: 28 },
-  { name: 'Adventure', id: 12 },
-  { name: 'Animation', id: 16 },
-  { name: 'Comedy', id: 35 },
-  { name: 'Crime', id: 80 },
-  { name: 'Documentary', id: 99 },
-  { name: 'Drama', id: 18 },
-  { name: 'Family', id: 10751 },
-  { name: 'Fantasy', id: 14 },
-  { name: 'History', id: 36 },
-  { name: 'Horror', id: 27 },
-  { name: 'Music', id: 10402 },
-  { name: 'Mystery', id: 9648 },
-  { name: 'Romance', id: 10749 },
-  { name: 'Sci-Fi', id: 878 },
-  { name: 'TV Movie', id: 10770 },
-  { name: 'Thriller', id: 53 },
-  { name: 'War', id: 10752 }
-];
 
-  function fetchGenres() {
-  const genreContainer = document.getElementById('genreContainer');
 
-  genres.forEach((genre) => {
-    const genreLink = document.createElement('a');
-    genreLink.href = `${BASE_URL}/discover/movie?sort_by=popularity.desc&with_genres=${genre.id}&${API_KEY}`;
-    genreLink.textContent = genre.name;
-    genreLink.classList.add('genre1');
-    genreLink.target = '_blank';
-    genreContainer.appendChild(genreLink);
-  });
-}
+  
 
-fetchGenres();
+
 
 
 getMovies(API_URL);
